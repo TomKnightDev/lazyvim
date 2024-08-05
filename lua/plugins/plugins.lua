7fb81d99f7d50d9183cc3d9ad880eebb86a9d1c0
Tom Knight <tomknight.dev@gmail.com>
2024-08-05 11:02:44 +0100

    Update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,3 +1,27 @@
 return {
   { "https://github.com/s1n7ax/nvim-window-picker", ft = "lua" },
+  { "https://github.com/tpope/vim-dadbod" },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  { "https://github.com/jackMort/ChatGPT.nvim" },
+  { "neovim/nvim-lspconfig", opts = {
+    inlay_hints = {
+      enabled = false,
+    },
+  } },
 }
